a7c6b002e3c3598295a93f1970c9ddbe57da9a1f
kibochamark <kibochamark@gmail.com>
2024-12-08 11:20:38 +0000

    Build:created database schema

---
+++ dist/src/index.js
@@ -0,0 +1,50 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var treblle_1 = require("treblle");
+var dotenv_1 = __importDefault(require("dotenv"));
+var cors_1 = __importDefault(require("cors"));
+var cookie_parser_1 = __importDefault(require("cookie-parser"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var compression_1 = __importDefault(require("compression"));
+dotenv_1.default.config();
+var app = (0, express_1.default)();
+// setting up treblle for api monitoring
+(0, treblle_1.useTreblle)(app, {
+    apiKey: process.env.TREBLLE_API_KEY,
+    projectId: process.env.TREBLLE_APP_ID,
+});
+app.use((0, cors_1.default)());
+app.use((0, cookie_parser_1.default)());
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, compression_1.default)());
+// catch all routes that are not specified in the routes folder
+app.use('*', function (req, res, next) {
+    var error = new Error("Cannot find ".concat(req.originalUrl, " on the server"));
+    error.status = 'fail';
+    error.statusCode = 404;
+    next(error); // Forward the error to the global error handler
+});
+// create our global error handler
+app.use(function (error, req, res, next) {
+    // Define default values for error
+    var statusCode = error.statusCode || 500;
+    var status = error.status || 'error';
+    res.status(statusCode).json({
+        status: status,
+        message: error.message,
+    });
+});
+app.get('/', function (req, res) {
+    var name = process.env.NAME || 'World';
+    res.send("Hello ".concat(name, "!"));
+});
+var port = parseInt(process.env.PORT || '3000', 10);
+app.listen(port, function () {
+    console.log("Listening on port ".concat(port));
+});
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,62 @@
 import express from 'express';
+import { useTreblle } from 'treblle';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import { GlobalError } from '../types/errorTypes';
+
+dotenv.config();
+
 const app = express();
+
+// setting up treblle for api monitoring
+useTreblle(app, {
+  apiKey: process.env.TREBLLE_API_KEY!,
+  projectId: process.env.TREBLLE_APP_ID!,
+});
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression());
+
+// catch all routes that are not specified in the routes folder
+app.use('*', (req, res, next) => {
+  const error: GlobalError = new Error(`Cannot find ${req.originalUrl} on the server`) as GlobalError;
+  error.status = 'fail';
+  error.statusCode = 404;
+
+  next(error); // Forward the error to the global error handler
+});
+
+// create our global error handler
+app.use(
+  (
+    error: GlobalError,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    // Define default values for error
+    const statusCode = error.statusCode || 500;
+    const status = error.status || 'error';
+
+    res.status(statusCode).json({
+      status,
+      message: error.message,
+    });
+  }
+);
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
   res.send(`Hello ${name}!`);
 });
 
-const port = parseInt(process.env.PORT || '3000');
+const port = parseInt(process.env.PORT || '3000', 10);
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
